82739735df0985e618b5458de445acf64f4a8314
Pushpendra Singh <154301045+Pushp11721@users.noreply.github.com>
2025-10-10 21:04:37 +0530

    Add files via upload

---
+++ Day30/NATO/main.py
@@ -0,0 +1,36 @@
+#TODO : upgrade NATO project for errors
+#What if we enter a number in input instead of a string? - It will give us a KeyError
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+# wrong = True
+# while wrong:
+#     word = input("Enter a word: ").upper()
+#     try:
+#         output_list = [phonetic_dict[letter] for letter in word]
+#     except KeyError:
+#         print("Sorry, only letters in the alphabet please.")
+#     else:
+#         print(output_list)
+#         wrong = False
+
+#TODO : OR
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
+
+#TODO : go back to password manager and apply this
